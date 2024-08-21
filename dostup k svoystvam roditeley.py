525bc26a4e6a90b4fff13ae8c25e1bf344457b31
koostyan <165835474+koostyan@users.noreply.github.com>
2024-08-21 14:39:28 +0300

    Add files via upload

---
+++ dostup k svoystvam roditeley.py
@@ -0,0 +1,39 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['Red', 'Blue', 'White', '<Black>']
+
+    def __init__(self, owner: str, model: str, engine_power: int, color: str):
+        self.owner = owner
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
+
+    def get_model(self):
+        return f"Модель: {self.__model}"
+
+    def get_horsepower(self):
+        return f"Мощность двигателя: {self.__engine_power}"
+
+    def get_color(self):
+        return f"Цвет: {self.__color}"
+
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f"Владелец: {self.owner}")
+
+    def set_color(self, new_color: str):
+        if new_color.capitalize() in [color.capitalize() for color in self.__COLOR_VARIANTS]:
+            self.__color = new_color
+        else:
+            print(f"Нельзя сменить цвет на {new_color}")
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+
+sedan = Sedan("Fedos", "Toyota Mark II", 500, "blue")
+sedan1 = Sedan("Vasyok", "Toyota Mark II", 500, "BLACK")
+sedan.print_info()
+sedan.set_color("Pink")
+sedan1.print_info()
